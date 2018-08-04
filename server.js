ff9bf93b04ba98e04d2473984dc801cc8e03bcb1
Erik Pontifex <erikpon@gmail.com>
2018-08-03 19:47:08 -0700

    added domain cert to server.js

--- server.js
+++ server.js
@@ -6,9 +6,14 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist/client')));
 
+app.get('/.well-known/acme-challenge/nWNWMV-GII2Ak3U6QHhQLk1B_GeHS9KDEXFK6J90r_U', (req, res)=>{
+  res.send('nWNWMV-GII2Ak3U6QHhQLk1B_GeHS9KDEXFK6J90r_U.r3cuzAVFNKO-N1Nx4Zczz9QxsJTsZf1Gbo39g7ydh8U');
+ })
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/client/index.html'));
 });
+
 
 const port = process.env.PORT || 3000;
 app.set('port', port);
