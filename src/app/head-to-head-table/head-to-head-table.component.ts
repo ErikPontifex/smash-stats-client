8a92bda802c894bb46b9d627a4f0294961c1ce5c
Erik Pontifex <erikpon@gmail.com>
2018-08-09 17:48:08 -0700

    deleted animations on head-to-head table

--- src/app/head-to-head-table/head-to-head-table.component.ts
+++ src/app/head-to-head-table/head-to-head-table.component.ts
@@ -9,13 +9,6 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
   selector: 'head-to-head-table',
   templateUrl: './head-to-head-table.component.html',
   styleUrls: ['./head-to-head-table.component.css'],
-  animations: [
-    trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
-      state('expanded', style({height: '*'})),
-      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-    ]),
-  ],
 })
 export class HeadToHeadTableComponent implements OnInit {
 
